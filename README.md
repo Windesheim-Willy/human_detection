2e94f367263526a4c305040992733a8f754b155b
Johnny Borg <johnnyborg@hotmail.com>
2018-11-27 14:19:02 +0100

    updated make list

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # human_detection
-
+$ apt get install ros-kinetic-opencv3
 
 # cascade downloads
 https://github.com/opencv/opencv/tree/master/data/haarcascades
+
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 int main(int argc, const char** argv)
 {
         // prepare video input
-        VideoCapture cap("/dev/video0");
+        VideoCapture cap("test.mov");
 
         CascadeClassifier detectBody;
         string cascade = "opencv/cascade.xml";
