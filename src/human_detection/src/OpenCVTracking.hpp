5aed0b5b0d9d736d1cbe7efc88bb5ff344f9dc15
Your Name <johnnyborg@hotmail.com>
2018-12-04 15:56:52 +0100

    added opencv tracking class

---
+++ src/human_detection/src/OpenCVTracking.hpp
@@ -0,0 +1,36 @@
+#ifndef HUMAN_DETECT_OPENCVTRACKING
+#define HUMAN_DETECT_OPENCVTRACKING
+
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/video/tracking.hpp"
+
+#include "Tracking/Rectangle.hpp"
+#include "Tracking/RectangleTracker.hpp"
+
+using namespace cv;
+using namespace std;
+
+class OpenCVTracking 
+{
+    public: 
+        OpenCVTracking(const string &filename);
+
+        bool isValid();
+        void process();
+
+    protected:
+        void drawRectange(Mat &image, Rectangle *rect);                
+
+    private:
+        VideoCapture *capture;
+        CascadeClassifier detectBody;
+
+        RectangleTracker bodyTracker;
+
+        bool valid;
+        char buffer[126];
+};
+
+#endif
