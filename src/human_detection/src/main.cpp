4d9f3b66d912b5050143c541927ac48443f02a62
Your Name <johnnyborg@hotmail.com>
2018-12-04 20:11:53 +0100

    working compile and catkin_make command

--- src/human_detection/src/main.cpp
+++ src/human_detection/src/main.cpp
@@ -1,11 +1,26 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
 #include "OpenCVTracking.hpp"
+#include "human_detection/Person.h"
 
-int main(int argc, const char** argv)
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "human_dect");
+    ros::NodeHandle n;
+
+    ros::Publisher chatter_pub = n.advertise<Person>("chatter", 1000);
+
     OpenCVTracking tracker("test-2-pers.mp4");
 
-    while(tracker.isValid()) 
+    while(tracker.isValid() && ros::ok()) 
     {
-        tracker.process();          
+        tracker.process();   
+
+        std_msgs::String msg;
+
+        msg.data = "hoi";
+        chatter_pub.publish(msg);
+
+        ros::spinOnce();       
     }
 }
