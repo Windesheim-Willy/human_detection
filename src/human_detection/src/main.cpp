c2d309229d8df0f22f392b247112a178028b24e2
Your Name <johnnyborg@hotmail.com>
2018-12-05 19:58:49 +0100

    working demo

--- src/human_detection/src/main.cpp
+++ src/human_detection/src/main.cpp
@@ -10,10 +10,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Publisher detectionTopic = n.advertise<human_detection::Person>("human_dect", 1);
 
-    OpenCVTracking tracker("test-2-pers.mp4");
-
-    cout << "starting \r\n";
-
+    OpenCVTracking tracker("/dev/video");
     human_detection::Person tracked; 
 
     while(tracker.isValid() && ros::ok()) 
