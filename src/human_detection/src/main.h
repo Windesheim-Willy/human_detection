c99f6dbf1912c0dff4cb49cdd41e9b7c26d94388
Johnny Borg <johnnyborg@hotmail.com>
2019-01-08 17:32:40 +0100

    default no debug

--- src/human_detection/src/main.h
+++ src/human_detection/src/main.h
@@ -1,7 +1,7 @@
 #ifndef HUMAN_DETECT_MAIN
 #define HUMAN_DETECT_MAIN
 
-#define DEBUG true
+#define DEBUG false
 #define VIDEO_INPUT "/dev/video0"
 #define CASCADE_PATH "../Documents/willy/components/human_detection/opencv/haar-upper-body.xml"
 #define FREQUENCY 10
